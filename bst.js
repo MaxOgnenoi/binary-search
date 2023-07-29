f01866f2a22312c55464e1c3e61a9fde525ba25d
Max Ognenoi <maksymohnenyi>
2023-07-29 17:25:44 -0400

    update

--- bst.js
+++ bst.js
@@ -1,63 +1,145 @@
 class Node {
     constructor(value) {
-        this.value = value
-        this.left = null
-        this.right = null
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
 }
 
 class BinarySearchTree {
     constructor() {
-        this.root = null
+        this.root = null;
     }
 
     insert(value) {
-        const newNode = new Node(value)
+        let newNode = new Node(value);
         if (this.root === null) {
-            this.root = newNode
-            return this
+            this.root = newNode;
+            return this;
         }
-        let current = this.root
+        let current = this.root;
         while (true) {
-            if (value === current.value) return undefined
+            if (value === current.value) return undefined;
             if (value < current.value) {
                 if (current.left === null) {
-                    current.left = newNode
-                    return this
+                    current.left = newNode;
+                    return this;
                 }
-                current = current.left
+                current = current.left;
             } else {
                 if (current.right === null) {
-                    current.right = newNode
-                    return this
+                    current.right = newNode;
+                    return this;
                 }
-                current = current.right
+                current = current.right;
             }
         }
     }
 
     find(value) {
-        if (this.root === null) return false
-        let current = this.root
-        let found = false
+        if (this.root === null) return false;
+        let current = this.root;
+        let found = false;
         while (current && !found) {
             if (value < current.value) {
-                current = current.left
+                current = current.left;
             } else if (value > current.value) {
-                current = current.right
+                current = current.right;
             } else {
-                found = true
+                found = true;
             }
         }
-        if (!found) return undefined
-        return current
+        if (!found) return undefined;
+        return current;
+    }
+    contains(value) {
+        if (this.root === null) return false;
+        let current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    bfs() {
+        let node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+    dfsPreOrder() {
+        let data = [];
+        function traverse(node) {
+            data.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    dfsPostOrder() {
+        let data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    dfsInOrder() {
+        let data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            data.push(node.value);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+    duplicate() {
+        let data = this.dfsPostOrder();
+        data.reverse();
+        let newtree = new BinarySearchTree();
+
+        data.forEach((num) => newtree.insert(num));
+        return newtree;
     }
 }
 
+
+
 const tree = new BinarySearchTree()
 
+tree.insert(20)
+tree.insert(27)
+tree.insert(22)
+tree.insert(21)
+tree.insert(23)
+tree.insert(19)
+tree.insert(17)
+tree.insert(12)
 tree.insert(5)
+tree.insert(7)
 tree.insert(4)
-tree.insert(6)
+
+console.log(tree.dfsInOrder())
+console.log(tree.duplicate().dfsInOrder())
 console.log(tree)
-console.log(tree.find(4))
+console.log(tree.duplicate())
